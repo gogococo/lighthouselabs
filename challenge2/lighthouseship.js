ffcc854c5fbfdb4392450edc2d112559011c033e
jacquelyneg <jacquelyne.grindrod@digiflare.com>
2019-05-04 15:06:46 -0400

    Challenge 4

--- challenge2/lighthouseship.js
+++ challenge2/lighthouseship.js
@@ -1,7 +1,27 @@
+var navigation = {
+  x: -2,
+  y: "Banana",
+  z: "Beep",
+  speed: "raaaaid"
+};
+
+var ship = {
+  powerOn: false,
+  modules: [],
+  antenna: {
+    active: false
+  }
+};
+
+var radio = {
+  frequency: "Kenneth",
+  message: "Bugs are cool.",
+  beacon: false
+};
+
 function powerOn() {
   ship.powerOn = true;
 }
-
 
 function countModules() {
   return availableModules.length;
@@ -16,6 +36,25 @@ function countEssential() {
   }
   return count;
 }
+
+function findModule(string) {
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].name === string) {
+      return i;
+    }
+  }
+}
+
+function loadModule(index) {
+  var moduleToAdd = availableModules[index]
+  moduleToAdd.enabled = true
+  ship.modules.push(moduleToAdd)
+}
+
+loadModule(findModule('life-support'))
+
+
+
 
 function countEssential2() {
   var count = availableModules.reduce((acc, module) => {
